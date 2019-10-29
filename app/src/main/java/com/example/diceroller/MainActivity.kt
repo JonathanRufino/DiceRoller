81d5e958cb402ba45ef8565b30bc539a7c60b214
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-10-29 00:28:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    // When you need to access a view from outside onCreate,
+    // declare it as a property and then initialize inside onCreate.
+    // It will prevent from finding the view multiple times
+    lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Do not declare diceImage = findViewById(R.id.dice_image) here
+        // because it will look through the view tree multiple times and will slow the app down
+        diceImage.setImageResource(drawableResource)
+    }
+}
